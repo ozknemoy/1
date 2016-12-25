a22f9c8ac4d00246d01c94e9358e3124088d92c3
ozknemoy <ozk.nemoy@mail.ru>
2016-12-25 13:05:37 +0400

    up

--- dev/src/common/factories/searchFactory.js
+++ dev/src/common/factories/searchFactory.js
@@ -4,11 +4,8 @@
 (function (angular) {
     angular.module("app")
         .factory('searchFactory', function ($http,diffdateFactory) {
-            //&key=AIzaSyCl_WJiupcodC3kxZ-fR2g-DEpgpYx23Q0
-            //006238157094868494689:xwaxko0z1y0
-            //006238157094868494689:4p-tpkktq1e
-            //014169764547731080872:l6vdhbguyw8
-            var baseUrl = 'https://www.googleapis.com/customsearch/v1?cx=014169764547731080872:l6vdhbguyw8&key=AIzaSyCl_WJiupcodC3kxZ-fR2g-DEpgpYx23Q0&searchType=image&num=9';
+            // вставить cx and key api
+            var baseUrl = 'https://www.googleapis.com/customsearch/v1?searchType=image&num=9';
             return function (Q,date,size,start) {
                 var num = 9;
                 Q = '&q=' + Q;
